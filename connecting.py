ea554a8d490beb3debd8e6f48377b2684b99b2a0
Diego <123476167+WhipAndLash@users.noreply.github.com>
2023-05-15 22:16:53 +0200

    Eksamen version
    Copy of my code for the check in system

---
+++ connecting.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+# Re-connect to the database with the password
+while True:
+    pwd = input("Enter the database password: ")
+    try:
+        conn = sqlite3.connect('testing.db')
+        conn.execute(f"PRAGMA key = '{pwd}';")
+        break
+    except:
+        print("Invalid password, please try again.")
+
+# Query the checkin table and print the results
+c = conn.cursor()
+c.execute("SELECT * FROM checkin")
+rows = c.fetchall()
+for row in rows:
+    print("Name:", row[0])
+    print("Course:", row[1])
+    print("Student-Id:", row[2])
+    print("List:", row[3])
+    print("")
+
+# Close the database connection
+conn.close()
---
+++ database.py
@@ -0,0 +1,43 @@
+import sqlite3
+import os
+
+filename = "testing.db"
+
+# Delete existing databases
+
+# check if file exists
+if os.path.isfile(filename):
+    # delete file
+    os.remove(filename)
+    
+# connecting to the database
+conn = sqlite3.connect('testing.db')
+
+# Prompt the user to set a password for the database
+password = input("Enter a password for the database: ")
+
+# Create a checkin table with a Name, Course, and Student-ID field
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS checkin
+             (Name TEXT, Course TEXT, Student_Id TEXT, List TEXT, Whitelist TEXT, Blacklist TEXT,  PRIMARY KEY (Name, Course, Student_Id, List))''')
+
+# IDs
+records = [('Diego', 'IMIT2', '1111', 'Whitelisted'),
+           ('Jack', 'IMST-IT2', '2222', 'Whitelisted'),
+           ('Bertine', 'IMIT2', '3333','Blacklisted'),
+           ('Theodor', 'IMST-IT2', '4444', 'Whitelisted'),
+           ('Isak', 'IMIT2', '5555','Blacklisted'),
+           ('Viktor', 'IMST-IT2', '6666', 'Whitelisted'),
+           ('Kristian', 'IMST-IT2', '7777', 'Whitelisted'),
+           ('Alice', 'IMST1', '8888','Blacklisted'),
+           ('Fred', 'IM1', '9999','Blacklisted'),
+           ('Eva', 'IMST1', '0000','Blacklisted')]
+
+# Insert the records into the checkin table
+c.executemany("INSERT INTO checkin (Name, Course, Student_Id, List) VALUES (?, ?, ?, ?)", records)
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
+
+
